b4ab0b228441f6fbaace3f6e3e48ee0ebbe9bb17
Vimbi <i.am.vimbi@gmail.com>
2023-04-22 09:51:08 +0300

    add function doc

--- src/utils/encrypt-password.ts
+++ src/utils/encrypt-password.ts
@@ -3,6 +3,12 @@ import { config } from 'dotenv';
 
 config();
 
+/**
+ * Encrypt password by bcryptjs with salt
+ * @param password password
+ * @returns return encrypted password
+ */
+
 export const encryptPassword = async (password: string) => {
   const saltRounds = process.env.SALT_ROUNDS;
   const salt = await bcrypt.genSalt(parseInt(saltRounds, 10));
